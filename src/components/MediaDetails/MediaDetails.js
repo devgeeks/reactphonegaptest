ae3c4067f3eded5004646b6db3f998589df8d981
Tommy-Carlos Williams <tommy@devgeeks.org>
2015-12-02 07:10:34 +1100

    Better media details page

--- src/components/MediaDetails/MediaDetails.js
+++ src/components/MediaDetails/MediaDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 // import classNames from 'classnames';
 
 import './index.less';
@@ -17,9 +18,20 @@ export default React.createClass({
     return (
       <div className="media-details">
         <div className="label">Collection Details</div>
-        <div className="">{ mediaItem.collectionCensoredName }</div>
-        <div className="">{ mediaItem.primaryGenreName }</div>
-        <div className="label">Preview</div>
+        <div className="collection-details">
+          <div className="row">
+            <div className="collection-name">{ mediaItem.collectionCensoredName }</div>
+            <div className="collection-price">${ mediaItem.collectionPrice }</div>
+          </div>
+          <div className="row">
+            <div className="collection-key">Released</div>
+            <div className="collection-value">{ moment(mediaItem.releaseDate, moment.ISO_8601).format('MMMM Do YYYY') }</div>
+          </div>
+          <div className="row">
+            <div className="collection-key">Genre</div>
+            <div className="collection-value">{ mediaItem.primaryGenreName }</div>
+          </div>
+        </div>
         <div />
       </div>
     );
